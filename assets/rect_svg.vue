8152432b85c0b432dae9ae01183003e8ce217393
Nodsader <nodsader@gmail.com>
2025-05-29 15:21:43 +0200

    Les calques sont tous présents, il ne manque plus que le CSS et le JS pour délivrer le rendu propre

---
+++ assets/rect_svg.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg width="100%" height="100%">
+    <defs>
+      <mask id="revealLogo">
+      <rect width="100%" height="100%" fill="white"/>
+      <path id="logoMask"></path>
+      </mask>
+    </defs>
+    <rect
+    width="100%"
+    height="100%"
+    fill="#11111"
+    mask="url(#revealLogo)"
+    />
+  </svg>
+</template>
--- components/hero.vue
+++ components/hero.vue
@@ -1,22 +1,43 @@
 <script setup lang="ts">
 
 import Arrow from "assets/arrow.vue";
+import rect_svg from "~/assets/rect_svg.vue";
+import Ps5_logo from "assets/ps5_logo.vue";
+import Xbox_logo from "assets/xbox_logo.vue";
 </script>
 
 <template>
   <section class="hero">
-    <div class="hero-img-container">
+    <div class="hero_img_container">
       <img src="/img/1.jpg" alt="artwork of Lucia and Jason"/>
     </div>
-    <div class="hero-img-logo">
+    <div class="hero_img_logo">
       <img src="/img/logo.png" alt="logo of Grand Theft Auto VI">
     </div>
     <img src="/img/mask.png" alt=""/>
 
-    <div>
+    <div class="fill-pink size-8 rotate-180">
       <arrow/>
     </div>
 
+    <div class="fade_overlay"></div>
+
+    <div class="overlay">
+      <rect_svg/>
+    </div>
+
+    <div class="logo_container"></div>
+
+    <div class="content_overlay">
+      <div class="logo_overlay">
+        <img src="/img/vi_logo.webp" alt=""/>
+      </div>
+      <h1>Available May 26, 2026</h1>
+      <div class="output_supports fill-main">
+        <ps5_logo/>
+        <xbox_logo/>
+      </div>
+    </div>
   </section>
   <section class="outro"></section>
 </template>
