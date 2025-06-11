90f48c0c2c5c54dda14860a4b4bdb249b9392b7a
Nodsader <nodsader@gmail.com>
2025-06-11 21:30:42 +0200

    Le hero commence à prendre doucement forme, le calque est bien positionné et le logo aussi

--- components/hero.vue
+++ components/hero.vue
@@ -14,13 +14,15 @@ import Xbox_logo from "assets/xbox_logo.vue";
     <div class="hero_img_logo">
       <img src="/img/logo.png" alt="logo of Grand Theft Auto VI">
     </div>
-    <img src="/img/mask.png" alt=""/>
+
 
     <div class="fill-pink size-8 rotate-180">
       <arrow/>
     </div>
 
-    <div class="fade_overlay"></div>
+    <div class="fade_overlay">
+      <img src="/img/mask.png" alt=""/>
+    </div>
 
     <div class="overlay">
       <rect_svg/>
@@ -39,9 +41,57 @@ import Xbox_logo from "assets/xbox_logo.vue";
       </div>
     </div>
   </section>
-  <section class="outro"></section>
+  <section class="outro">
+    <p>Looking forward to seeing what Rockstar has in store for us</p>
+  </section>
 </template>
 
 <style scoped>
+ img {
+   width: 100%;
+   height: 100%;
+   object-fit: cover;
+ }
 
+ h1 {
+   text-transform: uppercase;
+   font-size: 6rem;
+   font-weight: var(--font-weight-title);
+   letter-spacing: -0.1rem;
+   line-height: 0.9;
+ }
+
+ p {
+   text-transform: uppercase;
+   font-size: var(--font-weight-regular);
+   line-height: 0.9;
+ }
+
+ section {
+   position: relative;
+   width: 100vw;
+   height: 100vh;
+   background: var(--color-dark);
+   text-align: center;
+   overflow: hidden;
+ }
+
+ .hero_img_container, .hero_img_container img, .fade_overlay {
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 100%;
+ }
+
+ .hero_img_logo img {
+   position: absolute;
+   top: 5%;
+   left: 50%;
+   margin-left: 10%;
+   transform: translate(-50%, 0);
+   width: 900px;
+   height: auto;
+   object-fit: contain;
+ }
 </style>
